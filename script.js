cfe3526225ca0959d120348ff9d54d52590b5b33
Riyad Hasan <onlygobs0@gmail.com>
2025-03-18 15:51:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-const questions = document.querySelectorAll('.faq-question')
+const faqItems = document.querySelectorAll('.faq-items');
 
-questions.forEach((question) => {
+faqItems.forEach((faqItem) => {
+  const question = faqItem.querySelector('.faq-question');
+  const answer = faqItem.querySelector('.faq-answer');
+  const icon = faqItem.querySelector('.hide');
+  const plusIcon = icon.nextElementSibling;
+
   question.addEventListener('click', () => {
-    const answer = question.nextElementSibling;
-    answer.classList.toggle('open')
 
-    const plusIcon = question.querySelector('img + img')
-    const minusIcon = question.querySelector('img')
-    console.log(plusIcon, minusIcon)
+    // if(answer.style.maxHeight) {
+    //   answer.style.maxHeight = null;
+    // }
+    // else{
+    //   answer.style.maxHeight = answer.scrollHeight + 'px';
+    // }
 
+    answer.style.maxHeight = answer.style.maxHeight ? null : answer.scrollHeight + 'px';
+
+    // if(icon.classList.contains('hide')){
+    //   icon.classList.remove('hide')
+    //   plusIcon.classList.add('hide')
+    // }
+    // else{
+    //   icon.classList.add('hide')
+    //   plusIcon.classList.remove('hide')
+    // }
+
+    icon.classList.toggle('hide')
     plusIcon.classList.toggle('hide')
-    minusIcon.classList.toggle('show')
-  })
-})
+    
+  });
+
+});
